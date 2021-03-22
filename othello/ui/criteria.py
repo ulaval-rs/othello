d347ff746b16a4e0172f100a15d8b0f286c9de2f
Gabriel Couture <gacou54@gmail.com>
2021-03-22 15:13:22 -0400

    Handle when no file or directory is provided; minor refactoring to remove noise code

--- othello/ui/criteria.py
+++ othello/ui/criteria.py
@@ -17,51 +17,36 @@ class CriteriaTab(QtWidgets.QWidget):
 
         self.label_data_section = QtWidgets.QLabel(self)
         self.label_data_section.setGeometry(QtCore.QRect(10, 10, 81, 19))
-        self.label_data_section.setObjectName('label_data_section')
 
-        self.inline_file_to_add_criteria_filepath = QtWidgets.QLineEdit(self)
+        self.inline_file_to_add_criteria_filepath = QtWidgets.QLineEdit(self, text='')
         self.inline_file_to_add_criteria_filepath.setGeometry(QtCore.QRect(20, 60, 621, 31))
-        self.inline_file_to_add_criteria_filepath.setText('')
-        self.inline_file_to_add_criteria_filepath.setObjectName('file_to_add_criteria')
 
-        self.btn_browse_files_to_add = QtWidgets.QPushButton(self)
-        self.btn_browse_files_to_add.setGeometry(QtCore.QRect(650, 60, 103, 31))
-        self.btn_browse_files_to_add.setObjectName('btn_browse_files_to_add')
-        self.btn_browse_files_to_add.clicked.connect(self.browse_files_to_add)
+        self.btn_browse_geo_file = QtWidgets.QPushButton(self, clicked=self.browse_geo_file)
+        self.btn_browse_geo_file.setGeometry(QtCore.QRect(650, 60, 103, 31))
 
         self.label_file_to_add_criteria = QtWidgets.QLabel(self)
         self.label_file_to_add_criteria.setGeometry(QtCore.QRect(20, 40, 311, 19))
-        self.label_file_to_add_criteria.setObjectName('label_file_to_add_criteria')
 
         self.combobox_field_to_select = QtWidgets.QComboBox(self)
         self.combobox_field_to_select.setGeometry(QtCore.QRect(20, 120, 731, 31))
-        self.combobox_field_to_select.setObjectName('combobox_field_to_select')
 
         self.label_field_to_select = QtWidgets.QLabel(self)
         self.label_field_to_select.setGeometry(QtCore.QRect(20, 100, 311, 19))
-        self.label_field_to_select.setObjectName('label_field_to_select')
 
         self.label_macbeth_section = QtWidgets.QLabel(self)
         self.label_macbeth_section.setGeometry(QtCore.QRect(10, 170, 81, 19))
-        self.label_macbeth_section.setObjectName('label_macbeth_section')
 
         self.label_macbeth_file_to_select = QtWidgets.QLabel(self)
         self.label_macbeth_file_to_select.setGeometry(QtCore.QRect(20, 200, 311, 19))
-        self.label_macbeth_file_to_select.setObjectName('label_macbeth_file_to_select')
 
         self.inline_macbeth_filepath = QtWidgets.QLineEdit(self)
         self.inline_macbeth_filepath.setGeometry(QtCore.QRect(20, 220, 621, 31))
-        self.inline_macbeth_filepath.setText('')
-        self.inline_macbeth_filepath.setObjectName('inline_macbeth_filepath')
 
         self.btn_load_macbeth_file = QtWidgets.QPushButton(self)
         self.btn_load_macbeth_file.setGeometry(QtCore.QRect(650, 220, 103, 31))
-        self.btn_load_macbeth_file.setObjectName('btn_load_macbeth_file')
 
-        self.btn_add_column_to_file = QtWidgets.QPushButton(self)
+        self.btn_add_column_to_file = QtWidgets.QPushButton(self, clicked=self.write_file)
         self.btn_add_column_to_file.setGeometry(QtCore.QRect(610, 500, 151, 31))
-        self.btn_add_column_to_file.setObjectName('btn_add_column_to_file')
-        self.btn_add_column_to_file.clicked.connect(self.write_file)
 
         self.set_labels()
 
@@ -69,7 +54,7 @@ class CriteriaTab(QtWidgets.QWidget):
         self.label_data_section.setText('Données')
 
         self.label_file_to_add_criteria.setText('Sélectionner un fichier')
-        self.btn_browse_files_to_add.setText('Parcourir')
+        self.btn_browse_geo_file.setText('Parcourir')
 
         self.label_field_to_select.setText('Champ à transformer')
 
@@ -79,15 +64,17 @@ class CriteriaTab(QtWidgets.QWidget):
 
         self.btn_add_column_to_file.setText('Ajouter la colonne')
 
-    def browse_files_to_add(self):
+    def browse_geo_file(self):
         filepath = QtWidgets.QFileDialog.getExistingDirectory(self)
+        if filepath == '':
+            return
 
         try:
             self.df = gis.io.read(filepath)
             self.inline_file_to_add_criteria_filepath.setText(filepath)
             self.combobox_field_to_select.addItems(self.df.columns)
         except DriverError as e:
-            popup = Popup(f"Erreur de lecture du fichier : {e}"*80, self)
+            popup = Popup(f"Erreur de lecture du fichier : {e}", self)
             popup.show()
 
     def write_file(self):
