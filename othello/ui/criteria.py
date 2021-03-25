148586692fe1f8618816edaf4f80b2c56e05f6db
Gabriel Couture <gacou54@gmail.com>
2021-03-25 10:41:05 -0400

    Reading specific layer in gdb file

--- othello/ui/criteria.py
+++ othello/ui/criteria.py
@@ -1,6 +1,7 @@
 import configparser
 from typing import Optional, List
 
+import fiona
 import geopandas
 from PySide2 import QtWidgets, QtCore
 from fiona.errors import DriverError
@@ -18,6 +19,7 @@ class CriteriaTab(QtWidgets.QWidget):
     def __init__(self):
         self.df: Optional[geopandas.GeoDataFrame] = None
         self.macbeth_parser: Optional[MacbethParser] = None
+        self.geo_filepath: Optional[str] = None
 
         super().__init__()
 
@@ -36,11 +38,16 @@ class CriteriaTab(QtWidgets.QWidget):
         self.label_file_to_add_criteria = QtWidgets.QLabel(self)
         self.label_file_to_add_criteria.setGeometry(QtCore.QRect(20, 40, 311, 19))
 
-        self.combobox_field = QtWidgets.QComboBox(self)
-        self.combobox_field.setGeometry(QtCore.QRect(20, 120, 731, 31))
+        self.label_layer_to_select = QtWidgets.QLabel(self)
+        self.label_layer_to_select.setGeometry(QtCore.QRect(20, 100, 311, 19))
+        self.combobox_layer = QtWidgets.QComboBox(self)
+        self.combobox_layer.setGeometry(QtCore.QRect(20, 120, 340, 31))
+        self.combobox_layer.activated.connect(self.layer_has_been_selected)
 
         self.label_field_to_select = QtWidgets.QLabel(self)
-        self.label_field_to_select.setGeometry(QtCore.QRect(20, 100, 311, 19))
+        self.label_field_to_select.setGeometry(QtCore.QRect(400, 100, 311, 19))
+        self.combobox_field = QtWidgets.QComboBox(self)
+        self.combobox_field.setGeometry(QtCore.QRect(400, 120, 340, 31))
 
         self.label_macbeth_section = QtWidgets.QLabel(self)
         self.label_macbeth_section.setGeometry(QtCore.QRect(10, 170, 81, 19))
@@ -71,6 +78,7 @@ class CriteriaTab(QtWidgets.QWidget):
         self.label_file_to_add_criteria.setText('Sélectionner un fichier')
         self.btn_browse_geo_file.setText('Parcourir')
 
+        self.label_layer_to_select.setText('Couche à transformer')
         self.label_field_to_select.setText('Champ à transformer')
 
         self.label_macbeth_section.setText('MacBeth')
@@ -81,15 +89,22 @@ class CriteriaTab(QtWidgets.QWidget):
 
         self.btn_add_column_to_file.setText('Ajouter la colonne')
 
+    def layer_has_been_selected(self):
+        try:
+            self.df = gis.io.read(self.geo_filepath)
+            self.combobox_field.addItems(self.df.columns)
+        except DriverError as e:
+            popup = Popup(f"Erreur de lecture du fichier : {e}", self)
+            popup.show()
+
     def browse_geo_file(self):
-        filepath = QtWidgets.QFileDialog.getExistingDirectory(self)
-        if filepath == '':
+        self.geo_filepath = QtWidgets.QFileDialog.getExistingDirectory(self)
+        if self.geo_filepath == '':
             return
 
         try:
-            self.df = gis.io.read(filepath)
-            self.inline_file_to_add_criteria_filepath.setText(filepath)
-            self.combobox_field.addItems(self.df.columns)
+            self.combobox_layer.addItems(fiona.listlayers(self.geo_filepath))
+            self.inline_file_to_add_criteria_filepath.setText(self.geo_filepath)
         except DriverError as e:
             popup = Popup(f"Erreur de lecture du fichier : {e}", self)
             popup.show()
